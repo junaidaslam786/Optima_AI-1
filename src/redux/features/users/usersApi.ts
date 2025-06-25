eca236cf069d3e88a3890daedcafc3c5975f0059
ImaduddinZangi <imaduddinzangi07@gmail.com>
2025-06-24 19:30:45 -0700

    fix: Update usersApi to use anonKey for authorization and remove unused token handling

--- src/redux/features/users/usersApi.ts
+++ src/redux/features/users/usersApi.ts
@@ -1,17 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { User, CreateUser, UpdateUser } from "./usersTypes";
-import { RootState } from "@/redux/store";
 
 const API_BASE_URL = process.env.NEXT_API_BASE_URL;
-
+const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const usersApi = createApi({
   reducerPath: "usersApi",
   baseQuery: fetchBaseQuery({
     baseUrl: `${API_BASE_URL}/users`,
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.token;
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
+    prepareHeaders: (headers) => {
+      if (anonKey) {
+        headers.set("authorization", `Bearer ${anonKey}`);
       }
       return headers;
     },
